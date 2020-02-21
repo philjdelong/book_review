ba041f6c76ae708f9418ba0a9bd46a2106c31580
Phil DeLong <philjdelong@gmail.com>
2020-02-21 11:09:33 -0700

    Pass tests

--- spec/features/users_can_search_book_reviews_spec.rb
+++ spec/features/users_can_search_book_reviews_spec.rb
@@ -7,14 +7,18 @@ RSpec.describe "As a user" do
             visit "/"
 
             fill_in :title, with: "the man who saw everything"
-            save_and_open_page
+       
             click_on "Find Book"
             expect(current_path).to eq('/search')
 
-            expect(page).to have_content(@book.title)
-            expect(page).to have_content(@book.author)
-            expect(page).to have_content(@book.subjects)
-            expect(@book.reviews).to eq(2)
+            expect(page).to have_content("The Man Who Saw Everything")
+            expect(page).to have_content("Deborah Levy")
+           
+            expect(page).to have_content("The Man Who Saw Everything,” which was longlisted for the Booker Prize, looks at masculinity through the perspective of a young historian who sneers at “authoritarian old men.")
+            expect(page).to have_content("2019-10-09")
+            
+            expect(page).to have_content("Deborah Levy’s latest novel, “The Man Who Saw Everything,” experiments with time travel, history and the endless complications of love.")
+            expect(page).to have_content("2019-10-15")
          end
       end
    end

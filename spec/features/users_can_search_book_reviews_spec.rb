9ea62252fcf93ff8a7b780fe90be277d0dae4b9c
Phil DeLong <philjdelong@gmail.com>
2020-02-21 10:46:51 -0700

    Write test

---
+++ spec/features/users_can_search_book_reviews_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "As a user" do
+   describe "when i visit the home page" do
+      describe "and enter a book title into the form" do
+         it "i see all of the books info" do
+            visit "/"
+
+            fill_in :title, with: "the man who saw everything"
+            save_and_open_page
+            click_on "Find Book"
+            expect(current_path).to eq('/search')
+
+            expect(page).to have_content(@book.title)
+            expect(page).to have_content(@book.author)
+            expect(page).to have_content(@book.subjects)
+            expect(@book.reviews).to eq(2)
+         end
+      end
+   end
+end
+
+# Then I should see the book's info
+# For that book I should see
+# - Title
+# - Author
+# - Genres
+# (Note: genres is referred to as "subjects" in the book search response)
+# I should also see:
+# - This book has 2 reviews from the New York Times
+#   (Note: reviews are the "summary" in the book review response)
+# - Review 1: ""The Man Who Saw Everything,"" which was longlisted for the Booker Prize, looks at masculinity through the perspective of a young historian who sneers at "authoritarian old men.""
+# Review Publication Date: 2019-10-09
+# - Review 2: "Deborah Levy's latest novel, "The Man Who Saw Everything," experiments with time travel, history and the endless complications of love."
+# Review Publication Date: 2019-10-15
